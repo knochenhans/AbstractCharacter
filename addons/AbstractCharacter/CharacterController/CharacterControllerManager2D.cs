947b51964d58115d2180fd2e26632b24b45ffc47
Andre Jonas <nipsky@googlemail.com>
2025-04-15 10:33:10 +0200

    Refactor CharacterControllerManager2D to improve null checks for _character and CharacterController

--- addons/AbstractCharacter/CharacterController/CharacterControllerManager2D.cs
+++ addons/AbstractCharacter/CharacterController/CharacterControllerManager2D.cs
@@ -10,6 +10,18 @@ public class CharacterControllerManager2D
     {
         _character = character;
 
+        if (_character == null)
+        {
+            Logger.Log("Character is null", Logger.LogTypeEnum.Error);
+            return;
+        }
+
+        if (_character.CharacterControllerScene == null)
+        {
+            Logger.Log("CharacterControllerScene is null", Logger.LogTypeEnum.Error);
+            return;
+        }
+
         CharacterController = _character.CharacterControllerScene.Instantiate() as AbstractCharacterController2D;
         CharacterController.ControlledCharacter = _character;
         // CharacterController.CharacterNoticed += OnCharacterControllerCharacterNoticed;
@@ -17,6 +29,25 @@ public class CharacterControllerManager2D
     }
 
     // public void OnCharacterControllerCharacterNoticed(AbstractCharacter2D player) => _character.OnCharacterControllerCharacterNoticed(player);
-    public void OnScanArea2DBodyEntered(Node2D body) => CharacterController.OnScanArea2DBodyEntered(body);
-    public void OnScanArea2DBodyExited(Node2D body) => CharacterController.OnScanArea2DBodyExited(body);
+    public void OnScanArea2DBodyEntered(Node2D body)
+    {
+        if (CharacterController == null)
+        {
+            Logger.Log("CharacterController is null", Logger.LogTypeEnum.Error);
+            return;
+        }
+
+        CharacterController.OnScanArea2DBodyEntered(body);
+    }
+
+    public void OnScanArea2DBodyExited(Node2D body)
+    {
+        if (CharacterController == null)
+        {
+            Logger.Log("CharacterController is null", Logger.LogTypeEnum.Error);
+            return;
+        }
+
+        CharacterController.OnScanArea2DBodyExited(body);
+    }
 }
